c01cae699ca8e65b28c6a4ef548d6e972600af05
Armaan-Ali-786 <137769196+Armaan-Ali-786@users.noreply.github.com>
2023-06-26 16:25:31 +0530

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
+
+
+export default function Navbar(props) {
+  return (
+    <div>
+      <nav className={`navbar navbar-expand-lg navbar${props.back} bg-${props.back}`} >
+  <div className="container-fluid" >
+    <a className="navbar-brand" to="/" style={{color: props.back == 'black'? 'white' : 'black'}}>Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" to="/" style={{color: props.back == 'black'? 'white' : 'black'}}>{props.title}</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" to="/" style={{color: props.back == 'black'? 'white' : 'black'}}>Link</a>
+        </li>
+      </ul>
+    </div>
+    <div className="form-check form-switch" >
+  <input className="form-check-input" type="checkbox" role="switch" onClick={props.toogleIt}  id="flexSwitchCheckDefault"/>
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault"> switch</label>
+</div>
+  </div>
+
+</nav>
+
+    </div>
+  )
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  about:PropTypes.string
+}
+Navbar.defaultProps = {
+  title : 'fill title here',
+  about : 'fill about here'
+}
+
+
